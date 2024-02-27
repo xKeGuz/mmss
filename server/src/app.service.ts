e46dcf9bdd79bea8a83691e91bcdc0f4e6f501c0
Juan Delgado <juandelgadog98@gmail.com>
2024-02-26 20:23:41 -0500

    :sparkles: feat(Welcome API): Added html welcome API

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `
+    <!DOCTYPE html>
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Welcome to the MMSS API REST</title>
+      </head>
+      <body>
+        <h1>Hello World!</h1>
+        <p>Welcome to the MMSS API REST</p>
+      </body>
+    </html>  
+    `;
   }
 }
